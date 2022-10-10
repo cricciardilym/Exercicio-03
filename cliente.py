8a0559c1a2e3013abe5be14a34f7c015eed7b5f4
Caio Ricciardi <79649870+cricciardilym@users.noreply.github.com>
2022-10-10 08:14:36 -0300

    att

--- cliente.py
+++ cliente.py
@@ -1,10 +1,12 @@
 from socket import *
 
-# O cliente deverá inicialmente enviar uma mensagem para o servidor (mensagem 1) solicitando a relação dos candidatos para os quais
+# O cliente deverá inicialmente enviar uma mensagem para o servidor (mensagem 1) solicitando a relação dos candidatos 
+# para os quais
 # devem ser digitados os votos.
 # Deverá aguardar a mensagem 2 e depois de receber a mesma, deverá solicitar ao usuário o número de
 # votos de cada candidato, bem como o número de votos nulos e em branco.
-# Após a digitação dos dados, o cliente deverá enviar a mensagem 3 com os nomes dos candidatos, exatamente como vieram do servidor
+# Após a digitação dos dados, o cliente deverá enviar a mensagem 3 com os nomes dos candidatos, exatamente como vieram 
+# do servidor
 # e seus votos, bem como o número de votos brancos e o número de votos nulos.
 # O cliente esperará a mensagem 4 para poder imprimir na tela a parcial da apuração.
 
